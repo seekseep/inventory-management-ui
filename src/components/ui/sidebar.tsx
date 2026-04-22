379a697163df4b0bd0509545cf8b408c244c1632
YOKOYAMA Taku <seekseep@gmail.com>
2026-04-22 17:49:36 +0900

    fix

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -89,9 +89,7 @@ function SidebarProvider({
 
   // Helper to toggle the sidebar.
   const toggleSidebar = React.useCallback(() => {
-    return isMobile
-      ? setOpenMobile((prev) => !prev)
-      : setOpen((prev) => !prev)
+    return isMobile ? setOpenMobile((prev) => !prev) : setOpen((prev) => !prev)
   }, [isMobile, setOpen, setOpenMobile])
 
   // Adds a keyboard shortcut to toggle the sidebar.
